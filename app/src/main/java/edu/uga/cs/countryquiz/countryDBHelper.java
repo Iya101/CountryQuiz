55f868157af54dc0bbc97dd99b87914fa2789f43
Iyanna Yapo <Iyannayapo123@gmail.com>
2024-03-26 11:21:42 -0400

    git is making me commit : ( will erase

--- app/src/main/java/edu/uga/cs/countryquiz/countryDBHelper.java
+++ app/src/main/java/edu/uga/cs/countryquiz/countryDBHelper.java
@@ -1,31 +1,51 @@
-package edu.uga.cs.countryquiz;
 
+package edu.uga.cs.countryquiz;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class countryDBHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "jobLeads.db";
+
+    private static final String DATABASE_NAME = "quizApp.db";
     private static final int DATABASE_VERSION = 1;
 
-    // Table Name
-    private static final String TABLE_JOB_LEADS = "job_leads";
+    // Table Names
+    private static final String TABLE_COUNTRIES = "countries";
+    private static final String TABLE_NEIGHBORS = "neighbors";
+    private static final String TABLE_QUIZZES = "quizzes";
 
-    // Column Names
+    // Common column names
     private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_COMPANY_NAME = "company_name";
-    private static final String COLUMN_PHONE = "phone";
-    private static final String COLUMN_URL = "url";
-    private static final String COLUMN_COMMENTS = "comments";
 
-    // Creating table query
-    private static final String CREATE_TABLE_JOB_LEADS = "CREATE TABLE "
-            + TABLE_JOB_LEADS + "("
-            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-            + COLUMN_COMPANY_NAME + " TEXT,"
-            + COLUMN_PHONE + " TEXT,"
-            + COLUMN_URL + " TEXT,"
-            + COLUMN_COMMENTS + " TEXT" + ")";
+    // Countries Table - column names
+    private static final String COLUMN_COUNTRY_NAME = "name";
+    private static final String COLUMN_CONTINENT = "continent";
+
+    // Neighbors Table - column names
+    private static final String COLUMN_COUNTRY_ID = "country_id";
+    private static final String COLUMN_NEIGHBOR_ID = "neighbor_id";
+
+    // Quizzes Table - column names
+    private static final String COLUMN_QUIZ_DATE = "quiz_date";
+    private static final String COLUMN_QUIZ_RESULT = "result";
+
+    // Table Create Statements
+    // Countries table create statement
+    private static final String CREATE_TABLE_COUNTRIES = "CREATE TABLE "
+            + TABLE_COUNTRIES + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_COUNTRY_NAME + " TEXT," + COLUMN_CONTINENT + " TEXT" + ")";
+
+    // Neighbors table create statement
+    private static final String CREATE_TABLE_NEIGHBORS = "CREATE TABLE "
+            + TABLE_NEIGHBORS + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_COUNTRY_ID + " INTEGER," + COLUMN_NEIGHBOR_ID + " INTEGER,"
+            + "FOREIGN KEY(" + COLUMN_COUNTRY_ID + ") REFERENCES " + TABLE_COUNTRIES + "(" + COLUMN_ID + "),"
+            + "FOREIGN KEY(" + COLUMN_NEIGHBOR_ID + ") REFERENCES " + TABLE_COUNTRIES + "(" + COLUMN_ID + "))";
+
+    // Quizzes table create statement
+    private static final String CREATE_TABLE_QUIZZES = "CREATE TABLE "
+            + TABLE_QUIZZES + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_QUIZ_DATE + " TEXT," + COLUMN_QUIZ_RESULT + " INTEGER" + ")";
 
     public countryDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -33,12 +53,19 @@ public class countryDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_TABLE_JOB_LEADS);
+        // creating required tables
+        db.execSQL(CREATE_TABLE_COUNTRIES);
+        db.execSQL(CREATE_TABLE_NEIGHBORS);
+        db.execSQL(CREATE_TABLE_QUIZZES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // Handle database version upgrades here
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_COUNTRIES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NEIGHBORS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUIZZES);
+        // create new tables
+        onCreate(db);
     }
 }
-
